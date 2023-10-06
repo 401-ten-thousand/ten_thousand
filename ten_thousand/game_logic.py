95127dc8b0ee87bf2a428bf00402aa7775b4d7d6
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-10-05 20:23:00 -0500

    implemented calculate_score for straight, 3 pairs, and trio plus

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -14,6 +14,31 @@ class GameLogic():
         return tuple(dice_roll)
 
     @staticmethod
-    def calculate_score(rolled_dice: tuple[int]) -> int:
-
-        return score
+    def calculate_score(dice_roll: tuple[int]) -> int:
+        if len(dice_roll) < 1 or len(dice_roll) > 6:
+            raise ValueError('can only calculate scores for 1-6 dice rolls')
+        counter = {}
+        c_score = 0
+        # make counter dictionary
+        for index, value in enumerate(dice_roll):
+            if value in counter:
+                counter[value] += 1
+            else:
+                counter[value] = 1
+        # find 6 unique values which means there is a straight
+        if len(counter.keys()) == 6 and all([True if value == 1 else False for value in counter.values()]):
+            c_score = 1500
+            return c_score
+        # find 3 unique values checking for 3 pairs
+        if len(counter.keys()) == 3 and all([True if value == 2 else False for value in counter.values()]):
+            c_score = 1500
+            return c_score
+        # {2: 4, 1: 1, 5: 1}
+        # find trio+
+        for key, value in counter.items():
+            if value >= 3:
+                constant = (value - 3) + 1
+                if key == 1:
+                    c_score += (key * 1000) * constant
+                else:
+                    c_score += (key * 100) * constant
