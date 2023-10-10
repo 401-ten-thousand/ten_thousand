6ce6b9bd9c270497d85e203fc30ad18d6d4406ab
jdabassett <jacobbassett@gmail.com>
2023-10-09 23:00:46 -0600

    mvp

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -7,9 +7,8 @@ class Game():
         self.unbanked_score = 0
         self.banked_score = 0
         self.rolled_dice = GameLogic.roll_dice(6)
-        self.rolled_dice_counter = Counter(self.rolled_dice)
         self.kept_dice = ()
-        self.round_num = 0
+        self.round_num = 1
 
     def end_game(self,discharge: str):
         if discharge == "dishonorable":
@@ -19,113 +18,106 @@ class Game():
         sys.exit()
 
     def input_begin_game(self,str_prompt):
-        count = 0
-
-        #TODO: while True:
-        while count < 20:
+        while True:
             # print prompt and collect input
             print(str_prompt)
             user_input = str(input("> "))
 
             # if y then start game
             if user_input.lower() == 'y':
-                return 1
-            # if n then exit game
-            elif user_input.lower() == 'n':
+                return
+            # default exit game if wrong input
+            else:
                 self.end_game('dishonorable')
-            count += 1
 
-        # incase count exceed limit, exit game
-        self.end_game('honorable')
+    def is_valid_input(self, user_input, tuple_user_input):
+        # input is all digits
+        if user_input.isdigit():
+            # if exception raised; continue to next round
+            try:
+                int_user_input_score = GameLogic.calculate_score(tuple_user_input)
+            except Exception:
+                return False
+
+            # if no scorable points or empty input; continue to next round
+            if int_user_input_score == 0:
+                return False
+
+            # make counter dictionary of input with integers as keys
+            counter_user_input = Counter(tuple_user_input)
+            # convert rolled dice into counter
+            counter_rolled_dice = Counter(self.rolled_dice)
+
+            # check if all input character are in rolled dice and less than rolled dice counts
+            for key, value in counter_user_input.items():
+                if key in counter_rolled_dice:
+                    # if input exceed proportion in rolled dice
+                    if value > counter_rolled_dice[key]:
+                        return False
+                # if input not in rolled dice
+                else:
+                    return False
+
+            # if passing all the above return tuple of new kept dice and their score
+            return True
+
+        # if not all digits
+        else:
+            return False
 
     def input_keep_quit(self, str_prompt):
-        count = 0
+        # print prompt and collect input
+        print(str_prompt)
 
-        #TODO: while True:
-        while count < 20:
-            # print prompt and collect input
-            print(str_prompt)
-            user_input = input("> ")
+        user_input = input("> ")
 
-            # should the game end
-            if user_input.lower() == 'q':
-                self.end_game('honorable')
+        tuple_user_input = tuple(int(char) for char in user_input)
 
-            # are all input characters convertable to digits
-            #TODO: edge case decimal
-            elif user_input.isdigit():
-                # make counter dictionary of input with integers as keys
-                #TODO: counter instance
-                dict_user_input = Counter([int(char) for char in user_input])
+        # should the game end
+        if user_input.lower() == 'q':
+            self.end_game('honorable')
 
-                # check if all input character are in rolled dice and less than rolled dice counts
-                bool_update_kept_dice = True
-                for key, value in dict_user_input.items():
-                    if key in self.rolled_dice_counter:
-                        if value > self.rolled_dice_counter[key]:
-                            bool_update_kept_dice = False
-                    else:
-                        bool_update_kept_dice = False
+        # if user input is valid
+        elif self.is_valid_input(user_input, tuple_user_input):
+            # update kept dice
+            self.kept_dice = self.kept_dice + tuple_user_input
+            # update unbanked score
+            self.unbanked_score = GameLogic.calculate_score(self.kept_dice)
 
-                # only update if all input character are in rolled dice and less than rolled dice counts
-                if bool_update_kept_dice:
-                    # update kept dice
-                    # TODO: remove counter use input instead
-                    for key, value in dict_user_input.items():
-                        tuple_to_extend = tuple([key]*value)
-                        self.kept_dice = self.kept_dice + tuple_to_extend
 
-                    # roll dice again
-                    self.rolled_dice = GameLogic.roll_dice(6 - len(self.kept_dice))
-                    self.rolled_dice_counter = Counter(self.rolled_dice)
-
-                    # update unbanked score
-                    self.unbanked_score = GameLogic.calculate_score(self.kept_dice)
-                #TODO: remove return
-                return
-
-            # increment to limit just in case this takes all day
-            count += 1
-
-        # incase count exceed limit, exit game
-        self.end_game('honorable')
+            # if no dice left to roll
+            if (6 - len(self.kept_dice)) == 0:
+                self.banked_score += self.unbanked_score
+                return False
+            #otherwise roll remaining dice
+            else:
+                self.rolled_dice = GameLogic.roll_dice(6 - len(self.kept_dice))
+                return True
+        else:
+            return False
 
     def input_roll_bank_quit(self,str_prompt):
-        count = 0
+        # print prompt and collect input
+        print(str_prompt)
+        user_input = input("> ")
 
-        # TODO: while True:
-        while count < 20:
-            # print prompt and collect input
-            print(str_prompt)
-            user_input = input("> ")
+        # should the game end
+        if user_input.lower() == 'q':
+            self.end_game('honorable')
 
-            # should the game end
-            if user_input.lower() == 'q':
-                self.end_game('honorable')
-                return False
+        # bank score
+        elif user_input.lower() == 'b':
+            print(f"You banked {self.unbanked_score} points in round {self.round_num}")
+            self.banked_score += self.unbanked_score
+            print(f"Total score is {self.banked_score} points")
+            return False
 
-            # bank score
-            elif user_input.lower() == 'b':
-                print(f"You banked {self.unbanked_score} points in round {self.round_num}")
-                self.banked_score += self.unbanked_score
-                print(f"Total score is {self.banked_score} points")
-                self.unbanked_score = 0
-                self.kept_dice = ()
-                self.rolled_dice = GameLogic.roll_dice(6)
-                self.rolled_dice_counter = Counter(self.rolled_dice)
-                return False
-
-            # roll dice
-            elif user_input.lower() == 'r':
-                self.rolled_dice = GameLogic.roll_dice(6-len(self.kept_dice))
-                self.rolled_dice_counter = Counter(self.rolled_dice)
-                return True
-
-            # increment to limit just in case this takes all day
-            count += 1
-
-        # incase count exceed limit, exit game
-        self.end_game('honorable')
+        # roll dice
+        elif user_input.lower() == 'r':
+            self.rolled_dice = GameLogic.roll_dice(6-len(self.kept_dice))
+            return True
+        else:
+            return False
 
     def round(self):
         # loop per round
@@ -134,35 +126,38 @@ class Game():
             print(f"Rolling {len(self.rolled_dice)} dice...")
             print(f'*** {" ".join([str(integer) for integer in self.rolled_dice])} ***')
             # keep or quit
-            #TODO: must keep at least once die and must be scoring die
-            #TODO: exit game if input unsatisfactory
-            self.input_keep_quit("Enter dice to keep, or (q)uit:")
+            continue_round = self.input_keep_quit("Enter dice to keep, or (q)uit:")
+            # if user input isn't is not valid or is non-scoring
+            if continue_round is False: break
+            # round continues
             print(f"You have {self.unbanked_score} unbanked points and {len(self.rolled_dice)} dice remaining")
             # roll, bank or quit
             continue_round = self.input_roll_bank_quit("(r)oll again, (b)ank your points or (q)uit:")
-        #TODO: remove return
-        return
 
     def play(self):
         # print welcome statement
         print("Welcome to Ten Thousand")
 
         # should we begin game
-        self.round_num = self.input_begin_game("(y)es to play or (n)o to decline")
+        self.input_begin_game("(y)es to play or (n)o to decline")
 
         # the game
-        #TODO: while True:
-        #TODO: twenty round max
-        while self.round_num > 0:
+        while self.round_num < 21:
             # begin round
             print(f"Starting round {self.round_num}")
 
             # each round
             self.round()
 
+            # roll new dice
+            self.unbanked_score = 0
+            self.kept_dice = ()
+            self.rolled_dice = GameLogic.roll_dice(6)
+
             # end of round increment
             self.round_num += 1
 
+        self.end_game('honorable')
 
 
 new_game = Game()
