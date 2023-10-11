7ccf7846743abf2d9628c3b1e9dd61c05112e5b9
Kiengchay Gomez <kphoutthavong@gmail.com>
2023-10-11 16:54:01 -0500

    implemented OOP

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,84 +1,79 @@
 import sys
 from ten_thousand.game_logic import GameLogic
 
+class Game:
+    def __init__(self):
+        self.current_score = 0
+        self.current_round = 0
+        self.bankable_points = 0
+        self.remaining_dice = 6
 
-def quit_game(current_score):
-    print(f'Thanks for playing. You earned {current_score} points')
-    end_game()
+    def quit_game(self):
+        if self.current_round == 0:
+            print('OK. Maybe another time')
+        else:
+            print(f'Thanks for playing. You earned {self.current_score} points')
+        sys.exit()
 
+    def calculate_banked_score(self, points):
+        self.bankable_points += points
 
-def calculate_banked_score(current_score, points):
-    return current_score + points
+    def select_dice_to_keep(self, current_roll: tuple):
+        print('Enter dice to keep, or (q)uit:')
+        user_input = input('> ')
+        selection = []
 
+        if user_input == 'q':
+            self.quit_game()
 
-def continue_round_new_roll(remaining_dice, current_score, current_round, bankable):
-    print(f'Rolling {remaining_dice} dice...')
-    dice_roll = GameLogic.roll_dice(remaining_dice)
-    print('***' + ' '.join(map(str, dice_roll)) + ' ***')
-    select_dice_to_keep(dice_roll, current_score, current_round, bankable)
+        for char in user_input:
+            if int(char) in current_roll:
+                selection.append(int(char))
 
-def select_dice_to_keep(current_roll: tuple, current_score, current_round, bankable) -> list:
-    """
-    Select dice player wants to keep for the next round.
-    """
-    print('Enter dice to keep, or (q)uit:')
-    user_input = input('> ')
-    selection = []
+        bankable = GameLogic.calculate_score(tuple(selection))
+        print(f'You have {bankable} unbanked points and {len(current_roll) - len(selection)} dice remaining')
+        print('(r)oll again, (b)ank your points or (q)uit:')
 
-    if user_input == 'q':
-        quit_game(current_score)
+        response = input('> ')
 
-    for char in user_input:
-        if int(char) in current_roll:
-            selection.append(int(char))
+        if response == 'b':
+            self.calculate_banked_score(bankable)
+            print(f'You banked {bankable} points in round {self.current_round}')
+            self.current_score += bankable
+            print(f'Total score is {self.current_score} points')
+            self.start_new_round()
+        elif response == 'r':
+            self.continue_round()
+        elif response == 'q':
+            self.quit_game()
 
-    bankable += GameLogic.calculate_score(tuple(selection))
-    print(f'You have {bankable} unbanked points and {len(current_roll) - len(selection)} dice remaining')
-    print('(r)oll again, (b)ank your points or (q)uit:')
+    def start_new_round(self):
+        self.current_round += 1
+        print(f'Starting round {self.current_round}')
+        print(f'Rolling 6 dice... ')
+        dice_roll = GameLogic.roll_dice(6)
+        print('*** ' + ' '.join(map(str, dice_roll)) + ' ***')
+        self.select_dice_to_keep(dice_roll)
 
-    response = input('> ')
+    def continue_round(self):
+        pass
+        print(f'Rolling {self.remaining_dice} dice...')
+        dice_roll = GameLogic.roll_dice(self.remaining_dice)
+        print('***' + ' '.join(map(str, dice_roll)) + ' ***')
+        self.select_dice_to_keep(dice_roll)
 
-    if response == 'b':
-        new_score = calculate_banked_score(current_score, bankable)
-        print(f'You banked {bankable} points in round {current_round}')
-        print(f'Total score is {new_score} points')
-        start_new_round(new_score, current_round)
-    elif response == 'r':
-        # start_new_round(current_score, current_round)
-        remaining_dice = len(current_roll) - len(selection)
-        continue_round_new_roll(remaining_dice, current_score, current_round, bankable)
-    elif response == 'q':
-        quit_game(current_score)
+    def start_game(self):
+        print('Welcome to Ten Thousand\n(y)es to play or (n)o to decline')
+        response = input('> ')
 
-
-def start_new_round(current_score, current_round=0, bankable=0):
-    current_round += 1
-    print(f'Starting round {current_round}')
-    print(f'Rolling 6 dice...')
-    dice_roll = GameLogic.roll_dice(6)
-    print('*** ' + ' '.join(map(str, dice_roll)) + ' ***')
-    select_dice_to_keep(dice_roll, current_score, current_round, bankable)
-
-
-def start_game():
-    current_score = 0
-    print('Welcome to Ten Thousand\n(y)es to play or (n)o to decline')
-    response = input('> ')
-
-    if response.lower() == 'y':
-        start_new_round(current_score)
-    elif response.lower() == 'n':
-        print('OK. Maybe another time')
-        end_game()
-
-
-def main():
-    start_game()
-
-
-def end_game():
-    sys.exit()
+        if response.lower() == 'y':
+            self.start_new_round()
+        elif response.lower() == 'n':
+            self.quit_game()
 
 
 if __name__ == "__main__":
-    main()
+    game = Game()
+    game.start_game()
+
+
